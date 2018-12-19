1d63fa02141723934fd64d6741dc21b8c4d070a9
zhanghui.chen <13126618000@163.com>
2018-12-19 17:59:21 +0800

    learn

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
+import 'babel-polyfill'
+import './assets/styles/reset.css'
+import './assets/styles/reset.css'
 
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
